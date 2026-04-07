b85593a7d47345807f995277c0ba2b8bad6c7317
Vaghela-Meetraj <vaghelameetraj43@gmail.com>
2026-04-07 14:32:37 +0530

    Added SensorPanel component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,30 @@
 import ControlPanel from './components/Dashboard/ControlPanel'
 import TelemetryDisplay from './components/Dashboard/TelemetryDisplay'
+import SensorPanel from './components/Sensors/SensorPanel'
 import { useWebSocket } from './hooks/useWebSocket'
 
 function App() {
 
   const { connected, move, stop, reset } = useWebSocket()
 
-  const robotState = { mode: "manual" } // temp
+  const robotState = { mode: "manual" }
+
+  const imuData = {
+    orientation: { roll: 10, pitch: 5, yaw: 45 },
+    acceleration: { x: 0.5 },
+    gyroscope: { y: 0.2 }
+  }
+
+  const encoderData = {
+    left: { rpm: 120 },
+    right: { rpm: 110 },
+    totalDistance: 12
+  }
+
+  const lidarData = [
+    { hit: true, distance: 2.3 },
+    { hit: true, distance: 1.8 }
+  ]
 
   return (
     <div>
@@ -21,6 +39,12 @@ function App() {
       <TelemetryDisplay 
         robotState={robotState}
         connected={connected}
+      />
+
+      <SensorPanel
+        imuData={imuData}
+        encoderData={encoderData}
+        lidarData={lidarData}
       />
     </div>
   )
